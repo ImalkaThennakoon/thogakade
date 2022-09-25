a9b27c8b081a77a762c990a8794e6410630939f1
Imalka <imalkathennakoon11@gmail.com>
2022-09-26 02:57:42 +0530

    2

--- src/com/seekerscloud/pos/controler/CustomerFormConroller.java
+++ src/com/seekerscloud/pos/controler/CustomerFormConroller.java
@@ -1,4 +1,58 @@
 package com.seekerscloud.pos.controler;
 
+import com.seekerscloud.pos.db.Database;
+import com.seekerscloud.pos.modal.Customer;
+import com.seekerscloud.pos.view.tm.CustomerTm;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class CustomerFormConroller {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+    public TableView<CustomerTm> tblCustomer;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+    public TableColumn colOptions;
+
+    public void initialize(){
+       colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+       colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+       colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+       colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+       colOptions.setCellValueFactory(new PropertyValueFactory<>("btn"));
+
+        searchCustomers();
+    }
+
+    private void searchCustomers() {
+        ObservableList<CustomerTm> tmList = FXCollections.observableArrayList();
+        for(Customer c:Database.customerTable){
+            Button btn = new Button("Delete");
+            CustomerTm tm =new CustomerTm(c.getId(),c.getName(),c.getAddress(),c.getSalary(),btn);
+            tmList.add(tm);
+
+        }
+        tblCustomer.setItems(tmList);
+    }
+
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+        Customer c1 = new Customer(txtId.getText(),txtName.getText(),txtAddress.getText(),Double.parseDouble(txtSalary.getText()));
+
+       boolean isSaved = Database.customerTable.add(c1);
+       if(isSaved){
+           searchCustomers();
+           new Alert(Alert.AlertType.INFORMATION,"Customer Saved!").show();
+
+       }else{
+           new Alert(Alert.AlertType.INFORMATION,"Try Again").show();
+       }
+
+    }
 }
--- src/com/seekerscloud/pos/controler/DashboardFormController.java
+++ src/com/seekerscloud/pos/controler/DashboardFormController.java
@@ -1,9 +1,43 @@
 package com.seekerscloud.pos.controler;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DashboardFormController {
-    public void openCustomerFormOnAction(ActionEvent actionEvent) {
+    public AnchorPane dashboardContext;
+    public Label lblTime;
+    public Label lblDate;
+
+    public void initialize(){
+        setDateAndTime();
+    }
+
+    private void setDateAndTime() {
+        //set time
+        Timeline time = new Timeline(
+                new KeyFrame(Duration.ZERO,e->{
+                    DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    lblTime.setText(LocalDateTime.now().format(formatter));
+                }),new KeyFrame(Duration.seconds(1)));
+        time.setCycleCount(Animation.INDEFINITE);
+        time.play();
+    }
+
+    public void openCustomerFormOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("CustomerForm");
     }
 
     public void openItemFormOnAction(ActionEvent actionEvent) {
@@ -14,8 +48,10 @@ public class DashboardFormController {
 
     public void openOrderDetailsFormOnAction(ActionEvent actionEvent) {
     }
-    private void setUi(String Ui){
-
+    private void setUi(String ui) throws IOException {
+        Stage stage =(Stage) dashboardContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+ui+".fxml"))));
+        stage.centerOnScreen();
 
     }
 }
